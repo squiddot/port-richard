674f3453037d74f4419b4363ae1aa75c5f86b534
Richard Gil <richard@richardegil.com>
2022-08-02 17:35:57 -0500

    better video integration

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ import './global.scss'
 
 const currentDate = new Date().getFullYear();
 document.querySelector('footer span').innerHTML = currentDate
+
+
+const players = Array.from(document.querySelectorAll('.plyr__video-embed')).map((p) => new Plyr(p, { controls: ['play-large'], ratio: "h:w" } ));
+
+// const playersPortrait = Array.from(document.querySelectorAll('.plyr__video-embed.plyr__portrait')).map((p) => new Plyr(p, { controls: ['play-large'], vimeo: [{ portrait: true }] }));
