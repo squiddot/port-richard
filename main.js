3f8be6cb7e2af9285981af1e73d8945e53a621fe
Richard Gil <richard@richardegil.com>
2022-08-06 16:47:28 -0500

    near final content

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import './global.scss'
+import gsap from 'gsap';
 
 const currentDate = new Date().getFullYear();
 document.querySelector('footer span').innerHTML = currentDate
@@ -6,4 +7,19 @@ document.querySelector('footer span').innerHTML = currentDate
 
 const players = Array.from(document.querySelectorAll('.plyr__video-embed')).map((p) => new Plyr(p, { controls: ['play-large'], ratio: "h:w" } ));
 
-// const playersPortrait = Array.from(document.querySelectorAll('.plyr__video-embed.plyr__portrait')).map((p) => new Plyr(p, { controls: ['play-large'], vimeo: [{ portrait: true }] }));
+gsap.set(".hi", { xPercent: 10, yPercent: 10 });
+const cur = document.querySelector(".hi");
+const trigger = document.querySelector('h1 span');
+trigger.addEventListener("mouseenter", (e) => {
+  gsap.to(cur, 0, { autoAlpha: 1 });
+});
+
+trigger.addEventListener("mouseleave", (e) => {
+  gsap.to(cur, 0, { autoAlpha: 0 });
+});
+
+window.addEventListener("mousemove", (e) => {
+    const X = e.clientX;
+    const Y = e.clientY;
+    gsap.to(cur, 0, { x: e.pageX, y: e.pageY });
+});
